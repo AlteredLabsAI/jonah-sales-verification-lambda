fd34dbafe871a33177d4e179edaf805f9d50b2f9
Adam Buchen <adam@alteredlabs.ai>
2023-07-09 19:06:27 +0000

    AL-486 - first pass at writing verifiers for app store purchases

--- go.mod
+++ go.mod
@@ -5,14 +5,34 @@ go 1.18
 require github.com/AlteredLabsAI/jonah-shared v0.0.0-00010101000000-000000000000
 
 require (
+	cloud.google.com/go/compute v1.19.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/Integralist/go-elasticache v0.0.0-20190122104721-fb0aee05cd4e // indirect
+	github.com/awa/go-iap v1.20.0 // indirect
 	github.com/aws/aws-lambda-go v1.36.0 // indirect
 	github.com/aws/aws-sdk-go v1.44.125 // indirect
 	github.com/bradfitz/gomemcache v0.0.0-20221031212613-62deef7fc822 // indirect
+	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
 	github.com/integralist/go-findroot v0.0.0-20160518114804-ac90681525dc // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/api v0.118.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230403163135-c38d8f061ccd // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
 
 replace github.com/AlteredLabsAI/jonah-shared => ../jonah-shared
--- main.go
+++ main.go
@@ -1,13 +1,120 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	jshared "github.com/AlteredLabsAI/jonah-shared"
+	jsales "github.com/AlteredLabsAI/jonah-shared/sales"
+	"github.com/awa/go-iap/appstore"
+	"github.com/awa/go-iap/playstore"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+const APPSTORE_RESPONSE_STATUS_VALID int = 0
+
+const PLAYSTORE_PURCHASE_STATE_COMPLETED int64 = 0
+const PLAYSTORE_CONSUMPTION_STATE_UNCONSUMED int64 = 0
+const PLAYSTORE_ACKNOWLEDGEMENT_STATE_ACKNOWLEDGED int64 = 1
+
 func main() {
+	lambda.Start(handleVerificationRequest)
+}
 
-	fmt.Println(jshared.GenerateUUID())
+func handleVerificationRequest(ctx context.Context, input jsales.SalesVerificationRequestInput) error {
 
+	switch input.Platform {
+	case jshared.MOBILE_PLATFORM_NAME_IOS:
+		return handleAppStoreVerification(ctx, input)
+	case jshared.MOBILE_PLATFORM_NAME_ANDROID:
+		return handlePlayStoreVerification(ctx, input)
+	default:
+		return fmt.Errorf("invalid platform passed into handleVerificationRequest: %s", input.Platform)
+	}
+}
+
+func handleAppStoreVerification(ctx context.Context, input jsales.SalesVerificationRequestInput) error {
+
+	client := appstore.New()
+	req := appstore.IAPRequest{
+		ReceiptData: input.VerificationString,
+	}
+	resp := &appstore.IAPResponse{}
+	verifyErr := client.Verify(ctx, req, resp)
+	if verifyErr != nil {
+		return fmt.Errorf("could not verify purchase: %s", verifyErr.Error())
+	}
+
+	if resp.Environment == appstore.Sandbox {
+		fmt.Println("=== sandbox request received ===")
+	}
+
+	if resp.Status != APPSTORE_RESPONSE_STATUS_VALID {
+		return fmt.Errorf("unexpected status received from the app store: %d", resp.Status)
+	}
+
+	if resp.Receipt.BundleID != input.AppBundleID {
+		return fmt.Errorf("unexpected bundle ID received from the app store: %s", resp.Receipt.BundleID)
+	}
+
+	for _, item := range resp.Receipt.InApp {
+		if item.ProductID != input.ProductID {
+			continue
+		}
+
+		if jshared.CastStringToI(item.Quantity) == 0 {
+			return fmt.Errorf("unexpected item quantity received: %s", item.Quantity)
+		}
+
+		//The purchase was found and there's a valid quantity.
+		return nil
+	}
+
+	return fmt.Errorf("error: unable to find transaction matching product ID: %s", input.ProductID)
+}
+
+func handlePlayStoreVerification(ctx context.Context, input jsales.SalesVerificationRequestInput) error {
+
+	var envVar string
+	switch input.OrganizationID {
+	case jshared.ORGANIZATION_ID_ALTERSNAP:
+		envVar = "GOOGLE_ALTERSNAP_PUBLIC_KEY_JSON"
+	default:
+		return fmt.Errorf("invalid organization id - there is no public key available for %s", input.OrganizationID)
+	}
+
+	publicKeyJSONStr := jshared.GetLambdaEnv(envVar)
+	if publicKeyJSONStr == "" {
+		return fmt.Errorf("no %s was found", envVar)
+	}
+
+	playstoreClient, clientErr := playstore.New([]byte(publicKeyJSONStr))
+	if clientErr != nil {
+		return fmt.Errorf("could not initialize playstore client: %s", clientErr.Error())
+	}
+
+	purchase, verifyErr := playstoreClient.VerifyProduct(ctx, input.AppBundleID, input.ProductID, input.VerificationString)
+	if verifyErr != nil {
+		return fmt.Errorf("could not verify product: %s", verifyErr.Error())
+	}
+
+	if purchase.PurchaseState != PLAYSTORE_PURCHASE_STATE_COMPLETED {
+		return fmt.Errorf("invalid purchaseState: %d", purchase.PurchaseState)
+	}
+
+	if purchase.ConsumptionState != PLAYSTORE_CONSUMPTION_STATE_UNCONSUMED {
+		return fmt.Errorf("invalid consumptionState: %d", purchase.ConsumptionState)
+	}
+
+	if purchase.PurchaseToken != input.VerificationString {
+		return fmt.Errorf("invalid purchaseToken received. got %s, expected %s", purchase.PurchaseToken, input.VerificationString)
+	}
+
+	if purchase.AcknowledgementState != PLAYSTORE_ACKNOWLEDGEMENT_STATE_ACKNOWLEDGED {
+		return fmt.Errorf("invalid acknowledgementState: %d", purchase.AcknowledgementState)
+	}
+
+	//Possible TODO: Send developer payload upon purchase initiation and verify that it matches. (Maybe use userid?)
+
+	return nil
 }
